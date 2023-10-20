40f0b0bac48d876cbed846f2a545b83bacaddfd5
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2023-10-20 12:13:23 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,35 @@
-import requests
+#!/usr/bin/env python3
+"""
+web cach and tracker
+"""
 import redis
+import requests
 from functools import wraps
+from typing import Callable
 
-# Connect to the Redis server
-store = redis.Redis()
 
-def count_url_access(method):
-    """ Decorator counting how many times a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        # URL not in cache, fetch the content
-        html = method(url)
-
-        # Cache the result with an expiration time of 10 seconds
-        store.setex(cached_key, 10, html)
-
-        count_key = "count:" + url
-        # Increment the access count
-        store.incr(count_key)
-
-        return html
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper checks url's data is cached
+            and tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
-@count_url_access
+
+@track_get_page
 def get_page(url: str) -> str:
-    """ Returns HTML content of a URL """
-    res = requests.get(url)
-    return res.text
-
-if __name__ == "__main__":
-    # Example usage of the decorated get_page function
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"
-    html_content = get_page(url)
-    print(html_content)
-
-    # Check the number of times the URL was accessed
-    count_key = "count:" + url
-    access_count = store.get(count_key)
-    print(f'Access count for {url}: {access_count.decode("utf-8")}')
+    """ Makes http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text

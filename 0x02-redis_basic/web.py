d29f5aa65d858411999c77c66c540cf38e8479e0
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2023-10-20 12:08:37 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python3
-"""
-web cache and tracker
-"""
 import requests
 import redis
 from functools import wraps
 
+# Connect to the Redis server
 store = redis.Redis()
 
-
 def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+    """ Decorator counting how many times a URL is accessed """
     @wraps(method)
     def wrapper(url):
         cached_key = "cached:" + url
@@ -19,18 +14,32 @@ def count_url_access(method):
         if cached_data:
             return cached_data.decode("utf-8")
 
-        count_key = "count:" + url
+        # URL not in cache, fetch the content
         html = method(url)
 
+        # Cache the result with an expiration time of 10 seconds
+        store.setex(cached_key, 10, html)
+
+        count_key = "count:" + url
+        # Increment the access count
         store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+
         return html
     return wrapper
 
-
 @count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
+    """ Returns HTML content of a URL """
     res = requests.get(url)
     return res.text
+
+if __name__ == "__main__":
+    # Example usage of the decorated get_page function
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"
+    html_content = get_page(url)
+    print(html_content)
+
+    # Check the number of times the URL was accessed
+    count_key = "count:" + url
+    access_count = store.get(count_key)
+    print(f'Access count for {url}: {access_count.decode("utf-8")}')
